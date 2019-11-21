c29040e39cadd6d5b4b5ffa5b4b1b6511d0055a9
Annabel Lee <55999405+al610@users.noreply.github.com>
2019-11-20 23:29:12 -0800

    Add files via upload

---
+++ datasets.R
@@ -0,0 +1,3 @@
+# set up the data sets
+Sex_Occupatiosn_and_wages <- read.csv("SexandDetailedOccupation2016.csv", stringsAsFactors = FALSE)
+Seattle_Wages <- read.csv("Seattle_Wages.csv", stringsAsFactors = FALSE)
